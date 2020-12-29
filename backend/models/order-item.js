3c32b255599f4e4b667199501f8e11f5a508217b
Fadi Nouh <fadinoh@gmail.com>
2020-12-29 17:43:18 +0100

    update readme

---
+++ backend/models/order-item.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const orderItemSchema = mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+})
+
+exports.OrderItem = mongoose.model('OrderItem', orderItemSchema);
+
